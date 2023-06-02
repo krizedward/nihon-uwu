ab191763a126b779357be9a976a6056025fba54c
Edward Kristian Mangare, S.Kom <37824502+krizedward@users.noreply.github.com>
2023-06-02 14:04:07 +0700

    update route coming soon

--- v.10/routes/web.php
+++ v.10/routes/web.php
@@ -41,7 +41,7 @@ Route::prefix('admin')->group(function () {
 Route::get('/', function () {
     // return view('welcome');
     // return view('admin.dashboard');
-    return view('talent.dashboard');
+    return view('welcome');
 });
 
 Route::get('/old', [KategoriTalentController::class, 'index'])->name('admin.kategori-talent.index');
